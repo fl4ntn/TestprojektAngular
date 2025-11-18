a19d5ef1375a455549b86c1b16588429f6a659f7
fl4ntn <flynn.anton@gmx.de>
2025-11-18 13:19:19 +0100

    Update navbar.component.ts

--- src/app/landingPage/navbar/navbar.component.ts
+++ src/app/landingPage/navbar/navbar.component.ts
@@ -1,2 +1,13 @@
+import { Component } from '@angular/core';
 
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports:  [],
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./../../../styles.scss', './navbar.component.scss']
+})
+export class NavbarComponent {
+
+}
 
